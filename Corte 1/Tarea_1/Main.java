85961453cd16fe47b70ef40fcc66e7110c0b5dcf
cristianArenas2024 <nicolascruz30171@gmail.com>
2025-04-08 17:43:44 -0500

    Add files via upload

---
+++ Corte 1/Tarea_1/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+         Operations resultado = new Operations();
+         resultado.sumar(5,7);
+         System.out.println(resultado.sumar(5,7));
+         resultado.restar(7,3);
+         System.out.println(resultado.restar(7,3));
+         resultado.multiplicar(10, 1);
+         System.out.println(resultado.multiplicar(10, 1));
+         resultado.potencia(5, 2);
+         System.out.println(resultado.potencia(5, 2));
+         
+        }
+    
+}
---
+++ Corte 1/Tarea_1/Operations.java
@@ -0,0 +1,22 @@
+public class Operations {
+public int a,b,r;
+
+    public int sumar(int a, int b){
+        return a + b;
+    
+    }
+    
+    public int restar(int a, int b){
+       return a - b;
+
+    }
+    
+    public int multiplicar(int a, int b){
+        return a * b;
+    
+    }
+    
+    public int potencia(int base, int exponente){
+        return exponente == 0 ? 1: base*potencia(base,exponente-1);
+    }
+}
